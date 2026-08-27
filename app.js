590fe84b1648cbb880c9bcd64c770f5af3140d33
Doktah-QuyDuc <skaltersimpdoktah@gmail.com>
2026-08-27 15:36:27 +0700

    UPDATE LAB 2 W4

--- app.js
+++ app.js
@@ -58,3 +58,78 @@ function findMax(samples) {
 }
 
 console.log(findMax(samples));
+
+let card = document.querySelector(".section");
+
+console.log(card);
+
+card.textContent = "This card content has been changed";
+card.style.backgroundColor = "lightblue";
+card.style.color = "pink";
+card.style.fontSize = "50px";
+
+console.log(card);
+
+// let noti = document.createElement("div");
+// let message = document.createElement("p");
+
+// message.textContent = "This is a notification";
+// noti.appendChild(message);
+
+// document.body.appendChild(noti);
+
+let form = document.querySelector("#Submit");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let age = document.querySelector("#tuoi").value;
+
+  if (age === "" || Number(age) <= 0) {
+    let oldNoti = document.querySelector("#error_noti");
+
+    if (oldNoti) {
+      oldNoti.remove();
+    }
+
+    let noti = document.createElement("div");
+    noti.id = "error_noti";
+
+    let message = document.createElement("p");
+    message.textContent = "Age must be a positive number.";
+    message.style.color = "red";
+    message.style.fontWeght = "bold";
+    message.style.fontSize = "50px";
+    message.style.backgroundColor = "green";
+
+    noti.appendChild(message);
+    form.appendChild(noti);
+
+    setTimeout(function () {
+      noti.remove();
+    }, 5000);
+  } else {
+    let oldNoti = document.querySelector("#correct_noti");
+
+    if (oldNoti) {
+      oldNoti.remove();
+    }
+
+    let noti = document.createElement("div");
+    noti.id = "correct_noti";
+
+    let message = document.createElement("p");
+    message.textContent = "Ready to Submit";
+    message.style.color = "pink";
+    message.style.fontWeght = "bold";
+    message.style.fontSize = "50px";
+    message.style.backgroundColor = "blue";
+
+    noti.appendChild(message);
+    form.appendChild(noti);
+
+    setTimeout(function () {
+      noti.remove();
+    }, 5000);
+  }
+});
